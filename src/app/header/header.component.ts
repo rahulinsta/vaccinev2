c41c0d1e60ec03aa821193a23172943cec81381e
Rajan Tiwari <rajan@instaacoders.com>
2022-06-21 23:01:16 +0530

    UI of add vaccine Modal

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as bootstrap from 'bootstrap';
 
 @Component({
   selector: 'app-header',
@@ -12,10 +13,29 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-
+    var e = document.getElementById("navbar");
     this.uname = localStorage.getItem('ufname');
     console.log(this.uname);
 
+  }
+
+  toggleMenu(e: any) {
+    // console.log(e.currentTarget);
+    const btn = e.currentTarget;
+    btn.getAttribute('aria-expanded') == 'false' ? btn.setAttribute('aria-expanded', "true") : btn.setAttribute('aria-expanded', "false")
+    var myCollapse = document.getElementById('navbarSupportedContent')
+    // return new bootstrap.Collapse(myCollapse)
+    new bootstrap.Collapse(myCollapse!, {
+      toggle: false
+    })
+  }
+
+  addVaccine() {
+    var modalId = document.querySelector("#addVaccine");
+    var myModal = new bootstrap.Modal(modalId!, {
+      keyboard: false
+    })
+    myModal.show();
   }
 
 }
