205afaca27ea4fb360cf5c8a683d2a087ccd1fbc
Sudhanshu Tiwari <sudhanshut@instaacoders.com>
2022-07-05 17:50:28 +0530

    login

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormGroup, UntypedFormControl, Validators} from '@angular/forms';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 import { MainserviceComponent } from '../services/mainservice/mainservice.component';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {environment as env} from '../../environments/environment';
-import {Router} from "@angular/router"
-
+import {Router} from "@angular/router";
+import { passwordMatch } from './confirm-password.validator';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-signup',
@@ -21,21 +22,23 @@ export class SignupComponent implements OnInit {
   emailErr:any;
   passErr1:any;
   passErr2:any;
-  formArr={phone:'',email:'', fname:'', lname:'',emgNo:'',dob:'',password:'',cpassword:'',bloodGroup:'',gender:''};
+  emailRegex ='^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  // formArr={phone:'',email:'', fname:'', lname:'',emgNo:'',dob:'',password:'',cpassword:'',bloodGroup:'',gender:''};
   
-  form = new UntypedFormGroup({
-    phone: new UntypedFormControl('', [Validators.required]),
-    email: new UntypedFormControl('', [Validators.required, Validators.email]),
-    fname: new UntypedFormControl('', [Validators.required]),
-    lname: new UntypedFormControl('', [Validators.required]),
-    dob: new UntypedFormControl('', [Validators.required]),
-    emgNo: new UntypedFormControl('', [Validators.required]),
-    bloodGroup: new UntypedFormControl('', [Validators.required]),
-    password: new UntypedFormControl('', [Validators.required]),
-    cpassword: new UntypedFormControl('', [Validators.required]),
-    genderType: new UntypedFormControl('', [Validators.required])
-    
-  });
+  form = new FormGroup({
+    phone: new FormControl('', [Validators.required,  Validators.pattern("^[0-9]*$"),Validators.minLength(10)]),
+    email: new FormControl('', [Validators.required, Validators.pattern(this.emailRegex)]),
+    fname: new FormControl('', [Validators.required]),
+    lname: new FormControl('', [Validators.required]),
+    dob: new FormControl('', [Validators.required]),
+    emgNo: new FormControl('', [Validators.required]),
+    bloodGroup: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    cpassword: new FormControl('', [Validators.required]),
+    genderType: new FormControl('', [Validators.required])    
+  },
+  [passwordMatch('password', 'cpassword')]
+);
 
   constructor(private usrObj:MainserviceComponent,private http:HttpClient,private router: Router) { }
 
@@ -51,71 +54,70 @@ export class SignupComponent implements OnInit {
     return this.form.controls;
   }
   
-  submit(){
-    
+  submit(){    
     this.isSubmit = true;  
     this.isSubmitted = true;  
     if (this.form.invalid) {
       this.isSubmit = false;  
-
       return  
     }  
+    console.log(this.form.value);
     
 
-    this.formArr.phone = this.form.value.phone;
-    this.formArr.email = this.form.value.email;
-    this.formArr.fname = this.form.value.fname;
-    this.formArr.lname = this.form.value.lname;
-    this.formArr.dob = this.form.value.dob;
-    this.formArr.emgNo = this.form.value.emgNo;
-    this.formArr.bloodGroup = this.form.value.bloodGroup;
-    this.formArr.password = this.form.value.password;
-    this.formArr.cpassword = this.form.value.cpassword;
-    this.formArr.gender = this.form.value.genderType;
+    // this.formArr.phone = this.form.value.phone;
+    // this.formArr.email = this.form.value.email;
+    // this.formArr.fname = this.form.value.fname;
+    // this.formArr.lname = this.form.value.lname;
+    // this.formArr.dob = this.form.value.dob;
+    // this.formArr.emgNo = this.form.value.emgNo;
+    // this.formArr.bloodGroup = this.form.value.bloodGroup;
+    // this.formArr.password = this.form.value.password;
+    // this.formArr.cpassword = this.form.value.cpassword;
+    // this.formArr.gender = this.form.value.genderType;
 
    
-    this.usrObj.register(this.formArr).subscribe((data:any)=>{
-      console.log('step-1');
-      this.isSubmit = false;  
-      if (data.status){
-        this.successMsg = true;
-        this.successMsg = data.message;
-        this.form.reset();
-        this.isSubmitted = false;  
+    // this.usrObj.register(this.formArr).subscribe((data:any)=>{
+    //   console.log('step-1');
+    //   this.isSubmit = false;  
+    //   if (data.status){
+    //     this.successMsg = true;
+    //     this.successMsg = data.message;
+    //     this.form.reset();
+    //     this.isSubmitted = false;  
 
 
-        setTimeout(() => {
-          this.successMsg = false;
-          this.router.navigate(['/login']);
-        }, 3000);
+    //     setTimeout(() => {
+    //       this.successMsg = false;
+    //       this.router.navigate(['/login']);
+    //     }, 3000);
 
-        //this.router.navigate(['register-verify']);
-      }else{
-        console.log('yes inside the error message');
-        this.errmsg = true;
-        this.errmsg = data.message+data.data;
-        setTimeout(() => {
-          this.errmsg = false;
-        }, 3000);
+    //     //this.router.navigate(['register-verify']);
+    //   }else{
+    //     console.log('yes inside the error message');
+    //     this.errmsg = true;
+    //     this.errmsg = data.message+data.data;
+    //     setTimeout(() => {
+    //       this.errmsg = false;
+    //     }, 3000);
 
-        console.log(this.errmsg);
-      }
-    },
-    error=> {
-      if(error.error.errors.email){
-        this.emailErr =error.error.errors.email[0]; 
-      }
+    //     console.log(this.errmsg);
+    //   }
+    // },
+    // error=> {
+    //   if(error.error.errors.email){
+    //     this.emailErr =error.error.errors.email[0]; 
+    //   }
       
-      if(error.error.errors.password){
-        this.passErr1 =error.error.errors.password[0]; 
-        this.passErr2 =error.error.errors.password[1]; 
-      }
+    //   if(error.error.errors.password){
+    //     this.passErr1 =error.error.errors.password[0]; 
+    //     this.passErr2 =error.error.errors.password[1]; 
+    //   }
 
     
-      //this.errmsg  = 'Internal Server Error.. You Registration could not be completed.';
-      this.isSubmit = false;  
+    //   //this.errmsg  = 'Internal Server Error.. You Registration could not be completed.';
+    //   this.isSubmit = false;  
 
-    });
+    // });
 
     
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,13 @@ export const environment = {
   production: true,
   apiurl:'https://vaccine2.showmeproject.com/api/v2/',
   imgurl:'https://vaccine2.showmeproject.com/admin/public/storage/',
+  firebase: {
+    apiKey: "AIzaSyBM4iMSuQf68kUp4HLruFA1-deyW6Cn8Yc",
+    authDomain: "vaccines-proof.firebaseapp.com",
+    projectId: "vaccines-proof",
+    storageBucket: "vaccines-proof.appspot.com",
+    messagingSenderId: "199153932759",
+    appId: "1:199153932759:web:47323bf0556fe8857e9392",
+    measurementId: "G-Z0JSRQ70RK"
+  }
 };
