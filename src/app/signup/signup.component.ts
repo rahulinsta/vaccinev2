aedd94e1d16fb36438cc57673aeaa00d68326e24
Sudhanshu Tiwari <sudhanshut@instaacoders.com>
2022-07-05 18:31:41 +0530

    resolve conflict

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -5,7 +5,6 @@ import {HttpClient, HttpParams} from '@angular/common/http';
 import {environment as env} from '../../environments/environment';
 import {Router} from "@angular/router";
 import { passwordMatch } from './confirm-password.validator';
-import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-signup',
@@ -25,7 +24,6 @@ export class SignupComponent implements OnInit {
   emailRegex ='^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
   // formArr={phone:'',email:'', fname:'', lname:'',emgNo:'',dob:'',password:'',cpassword:'',bloodGroup:'',gender:''};
   
-<<<<<<< HEAD
   form = new FormGroup({
     phone: new FormControl('', [Validators.required,  Validators.pattern("^[0-9]*$"),Validators.minLength(10)]),
     email: new FormControl('', [Validators.required, Validators.pattern(this.emailRegex)]),
@@ -40,21 +38,6 @@ export class SignupComponent implements OnInit {
   },
   [passwordMatch('password', 'cpassword')]
 );
-=======
-  form = new UntypedFormGroup({
-    phone: new UntypedFormControl('', [Validators.required]),
-    email: new UntypedFormControl('', [Validators.required, Validators.email]),
-    fname: new UntypedFormControl('', [Validators.required]),
-    lname: new UntypedFormControl('', [Validators.required]),
-    dob: new UntypedFormControl('', [Validators.required]),
-    emgNo: new UntypedFormControl(''),
-    bloodGroup: new UntypedFormControl(''),
-    password: new UntypedFormControl('', [Validators.required]),
-    cpassword: new UntypedFormControl('', [Validators.required]),
-    genderType: new UntypedFormControl('', [Validators.required])
-    
-  });
->>>>>>> 3e63d95cc148c82e7beb04662b99fc85cf462582
 
   constructor(private usrObj:MainserviceComponent,private http:HttpClient,private router: Router) { }
 
@@ -92,7 +75,6 @@ export class SignupComponent implements OnInit {
     // this.formArr.gender = this.form.value.genderType;
 
    
-<<<<<<< HEAD
     // this.usrObj.register(this.formArr).subscribe((data:any)=>{
     //   console.log('step-1');
     //   this.isSubmit = false;  
@@ -124,39 +106,6 @@ export class SignupComponent implements OnInit {
     //   if(error.error.errors.email){
     //     this.emailErr =error.error.errors.email[0]; 
     //   }
-=======
-    this.usrObj.register(this.formArr).subscribe((data:any)=>{
-      console.log('step-1');
-      this.isSubmit = false;  
-      if (data.status){
-        this.successMsg = true;
-        this.successMsg = data.message;
-        this.form.reset();
-        this.isSubmitted = false;  
-
-
-        setTimeout(() => {
-          this.successMsg = false;
-          this.router.navigate(['/login']);
-        }, 3000);
-
-        this.router.navigate(['/login']);
-      }else{
-        console.log('yes inside the error message');
-        this.errmsg = true;
-        this.errmsg = data.message+data.data;
-        setTimeout(() => {
-          this.errmsg = false;
-        }, 3000);
-
-        console.log(this.errmsg);
-      }
-    },
-    error=> {
-      if(error.error.errors.email){
-        this.emailErr =error.error.errors.email[0]; 
-      }
->>>>>>> 3e63d95cc148c82e7beb04662b99fc85cf462582
       
     //   if(error.error.errors.password){
     //     this.passErr1 =error.error.errors.password[0]; 
