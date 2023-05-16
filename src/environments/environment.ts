e96006bd522056eea5db86504abfc8edbd8836be
Sudhanshu Tiwari <sudhanshut@instaacoders.com>
2023-05-16 12:36:53 +0530

    api url change to https://portal.mydigirecords.com/

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = { 
   production: true,
-  apiurl:'https://vaccine2.showmeproject.com/api/v2/',
-  imgurl:'https://vaccine2.showmeproject.com/admin/public/storage/',
+  apiurl:'https://portal.mydigirecords.com/api/v2/',
+  imgurl:'https://portal.mydigirecords.com/admin/public/storage/',
   firebase: {
     apiKey: "AIzaSyBM4iMSuQf68kUp4HLruFA1-deyW6Cn8Yc",
     authDomain: "vaccines-proof.firebaseapp.com",
